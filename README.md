275281199416a507ff7645a7aca483f793d7e6e6
MichaelG <gainmikey@gmail.com>
2021-11-03 15:57:32 -0400

    added readme and cli

---
+++ README.md
@@ -0,0 +1,6 @@
+#USEAGE
+
+./convertToAscii [imageFileName] [outputFileName]
+
+For example:
+./convertToAscii cat.jpg cat.txt
--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,17 @@ int main(int argc, char* argv[]) {
     std::string imageFile;
     std::string outputFile;
 
-    // imageFile = "trump.jpeg";
-    // outputFile = "trump.txt";
-
-    imageFile = argv[1];
-    outputFile = argv[2];
-    
-    // std::cin >> imageFile;
+    if(argc == 3) {
+        imageFile = argv[1];
+        outputFile = argv[2];
+    }
+    else {
+        std::cout << "Please enter the image to be processed then the output text file" << std::endl;
+        std::cin >> imageFile >> outputFile;
+    }
 
     AsciiTools::ImageData imageData = AsciiTools::loadImage(imageFile.c_str());
     AsciiTools::convertToAscii(imageData, outputFile, " _-*oOQA");
+
+    return 0;
 }
