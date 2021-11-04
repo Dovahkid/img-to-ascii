9053b511ab7dce98945948e3a541d5f6e166521d
Mikey <gainmikey@gmail.com>
2021-11-04 10:10:27 -0400

    dunno if this works

--- src/bin/imgToAscii.cpp
+++ src/bin/imgToAscii.cpp
@@ -23,11 +23,14 @@ void AsciiTools::convertToAscii(AsciiTools::ImageData imageData, std::string out
     int rgbCount = (imageData.height * imageData.width * 3);
     int aggregateValue = 0;
     int widthCounter = 0;
+	int fillLoc;
+	int colorRanges;
     std::fstream outFile;
     
     outFile.open(outputFile, std::ios::out);
-
-    for(int i = 0; i < rgbCount; i+=3) {
+	colorRanges = 256 / fillers.length();
+    
+	for(int i = 0; i < rgbCount; i+=3) {
         // printf(" | %d %d %d", imageData.image[i], imageData.image[i+1], imageData.image[i+2]);
         aggregateValue = (imageData.image[i] + imageData.image[i+1] + imageData.image[i+2]) / 3;
 
@@ -35,57 +38,38 @@ void AsciiTools::convertToAscii(AsciiTools::ImageData imageData, std::string out
             I would prefer if this wasnt hard coded but idc to find a better solution rn
         */
 
-		if(aggregateValue > 224)
-            outFile << fillers[0] << fillers[0];
+	   	// needs testing
+		for(int j = 0; j < fillers.length(); j++) {
+			if(aggregateValue > (256 - (j * colorRanges))) {
+				outFile << fillers[fillers.length()-j] << fillers[fillers.length()-j];
+			}
+		}
+
+	   outFile << fillers[fillLoc] << fillers[fillLoc];
+
+		// if(aggregateValue > 224)
+        //     outFile << fillers[0] << fillers[0];
             
-		else if(aggregateValue > 192)
-            outFile << fillers[1] << fillers[1];
+		// else if(aggregateValue > 192)
+        //     outFile << fillers[1] << fillers[1];
 
-		else if(aggregateValue > 160)
-            outFile << fillers[2] << fillers[2];
+		// else if(aggregateValue > 160)
+        //     outFile << fillers[2] << fillers[2];
 		
-		else if(aggregateValue > 128)
-            outFile << fillers[3] << fillers[3];
+		// else if(aggregateValue > 128)
+        //     outFile << fillers[3] << fillers[3];
 
-		else if(aggregateValue > 96)
-            outFile << fillers[4] << fillers[4];
+		// else if(aggregateValue > 96)
+        //     outFile << fillers[4] << fillers[4];
 
-		else if(aggregateValue > 64)
-            outFile << fillers[5] << fillers[5];
+		// else if(aggregateValue > 64)
+        //     outFile << fillers[5] << fillers[5];
         
-		else if(aggregateValue > 32)
-            outFile << fillers[6] << fillers[6];
+		// else if(aggregateValue > 32)
+        //     outFile << fillers[6] << fillers[6];
 
-		else if(aggregateValue > 0)
-            outFile << fillers[7] << fillers[7];
-
-/*
-		if(aggregateValue > 224)
-            printf("%c%c", fillers[0], fillers[0]);
-        
-		else if(aggregateValue > 192)
-            printf("%c%c", fillers[1], fillers[1]);
-
-		else if(aggregateValue > 160)
-            printf("%c%c", fillers[2], fillers[2]);
-		
-		else if(aggregateValue > 128)
-            printf("%c%c", fillers[3], fillers[3]);
-
-		else if(aggregateValue > 96)
-            printf("%c%c", fillers[4], fillers[4]);
-
-		else if(aggregateValue > 64)
-            printf("%c%c", fillers[5], fillers[5]);
-        
-		else if(aggregateValue > 32)
-            printf("%c%c", fillers[6], fillers[6]);
-
-		else if(aggregateValue > 0)
-            printf("%c%c", fillers[7], fillers[7]);
-
-		printf("%c", );
-*/
+		// else if(aggregateValue > 0)
+        //     outFile << fillers[7] << fillers[7];
         
         widthCounter++;
 
