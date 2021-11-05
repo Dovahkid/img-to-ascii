9db4a8a2f01b08649daf6188805269f74fea2537
MichaelG <gainmikey@gmail.com>
2021-11-04 21:15:20 -0400

    added fillers to args

--- src/bin/imgToAscii.cpp
+++ src/bin/imgToAscii.cpp
@@ -13,34 +13,34 @@ AsciiTools::ImageData AsciiTools::loadImage(const char* imageName) {
     int width, height, bpp;
     unsigned char* rgb_image = stbi_load(imageName, &width, &height, &bpp, 3);
 
-    // stbi_image_free(rgb_image);
+    
     return {rgb_image, width, height};
 }
 
 void AsciiTools::convertToAscii(AsciiTools::ImageData imageData, std::string outputFile, std::string fillers) {
 
     // 3 because it has 3 channels of color, RGB
-    int rgbCount = (imageData.height * imageData.width * 3);
+    const int rgbCount = (imageData.height * imageData.width * 3);
     int aggregateValue = 0;
     int widthCounter = 0;
-	int fillLoc;
 	int colorRange;
+    int fillerSize;
     std::fstream outFile;
     
+    fillerSize = fillers.length();
     outFile.open(outputFile, std::ios::out);
-	colorRange = 256 / fillers.length();
+	colorRange = 256 / fillerSize;
 
 	for(int i = 0; i < rgbCount; i+=3) {
-        // printf(" | %d %d %d", imageData.image[i], imageData.image[i+1], imageData.image[i+2]);
         aggregateValue = (imageData.image[i] + imageData.image[i+1] + imageData.image[i+2]) / 3;
 
-		for(int j = 1; j <= fillers.length(); j++) {
+		for(int j = 1; j <= fillerSize; j++) {
             if( aggregateValue > (256 - (j*colorRange)) ) {
                 outFile << fillers[j-1] << fillers[j-1];
                 break;
             }
         }
-        
+
         widthCounter++;
 
         if(widthCounter == imageData.width) {
--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,28 @@
 #include <string>
 
 int main(int argc, char* argv[]) {
-    std::string imageFile;
-    std::string outputFile;
+    std::string imageFile = "";
+    std::string outputFile = "";
+    std::string fillers = " _-*oOQA";
 
-    if(argc == 3) {
-        imageFile = argv[1];
-        outputFile = argv[2];
-    }
-    else {
-        std::cout << "Please enter the image to be processed then the output text file" << std::endl;
-        std::cin >> imageFile >> outputFile;
+    switch(argc) {
+        case 4:
+            fillers = argv[3];
+
+        case 3:
+            imageFile = argv[1];
+            outputFile = argv[2];
+        break;
+
+        default:
+            while(imageFile == "" || outputFile == "") {
+                std::cout << "Please enter the image to be processed then the output text file" << std::endl;
+                std::cin >> imageFile >> outputFile;
+            }
     }
 
     AsciiTools::ImageData imageData = AsciiTools::loadImage(imageFile.c_str());
-    AsciiTools::convertToAscii(imageData, outputFile, " _-*oOQA");
+    AsciiTools::convertToAscii(imageData, outputFile, fillers);
     
     return 0;
 }
