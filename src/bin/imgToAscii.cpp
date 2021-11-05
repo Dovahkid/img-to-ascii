e022ee2e9123385782c3a33eb4fecca3115ba01e
MichaelG <gainmikey@gmail.com>
2021-11-05 07:20:14 -0400

    idk

--- src/bin/imgToAscii.cpp
+++ src/bin/imgToAscii.cpp
@@ -13,7 +13,7 @@ AsciiTools::ImageData AsciiTools::loadImage(const char* imageName) {
     int width, height, bpp;
     unsigned char* rgb_image = stbi_load(imageName, &width, &height, &bpp, 3);
 
-    
+
     return {rgb_image, width, height};
 }
 
